22e9bc91c3fa2dbdc3e89eea5725d6d39fb956b6
Michaela Warady <mwarady22@berkeley.edu>
2020-06-15 10:24:22 -0700

    PollardsRhoMethod.py completed

---
+++ PollardsRhoMethod.py
@@ -0,0 +1,53 @@
+from sage.all import *
+import sys
+from EuclideanAlgorithm import *
+
+def Pollard(N): # N is a composite integer, will find a non-trivial factor of N
+	return initialize(N)
+
+def initialize(N):
+	return accumulate(N, 2, 2, 2, 1, 1, 1, 0)
+
+def accumulate(N, y, x, x1, k, l, P, c):
+	g = 1
+	x = (x**2 + 1) % N
+	P = (P * (x1 - x)) % N
+	c += 1
+	if c == 20:
+		g = gcd(P, N)
+		if g > 1:
+			return backtrack(N, y, g)
+		else:
+			y = x
+			c = 0
+	return advance(N, y, x, x1, k, l, P, c, g)
+
+def advance(N, y, x, x1, k, l, P, c, g):
+	k = k - 1
+	if k != 0:
+		return accumulate(N, y, x, x1, k, l, P, c)
+	else:
+		g = gcd(P, N)
+		if g > 1:
+			return backtrack(N, y, g)
+		else:
+			x1 = x
+			k = l
+			l = 2 * l
+			for j in range(0, k):
+				x = (x**2 + 1) % N
+			y = x
+			c = 0
+			return accumulate(N, y, x, x1, k, l, P, c)
+
+def backtrack(N, y, g):
+	while not (g > 1):
+		y = (y**2 + 1) % N
+		g = gcd(x1 - y, N)
+	if g < N:
+		return g
+	else:
+		return 'algorithm fails'
+
+
+# print(Pollard(419224629953))
