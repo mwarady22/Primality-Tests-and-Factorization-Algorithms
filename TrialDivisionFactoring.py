a7ffc9ab540ad7ecee1645f0ae5d181b1f72e5e9
Michaela Warady <mwarady22@berkeley.edu>
2020-06-19 12:49:04 -0700

    TrialDivisionFactoring.py bound issue fixed, now works with bound set, updated formatting

--- TrialDivisionFactoring.py
+++ TrialDivisionFactoring.py
@@ -2,7 +2,7 @@ from sage.all import *
 import sys
 from PrimesList import *
 
-t = [6, 4, 2, 4, 2, 4, 6, 2] # unknown why these numbers
+t = [6, 4, 2, 4, 2, 4, 6, 2]
 k = lastindex
 lastelem = primelist[lastindex] # the last element of the list of primes
 lastpmod = lastelem % 30
@@ -26,7 +26,13 @@ elif lastpmod == 29:
 
 
 def TrialFactor(N, B=lastelem): # Finds a factor of N of size up to B
-	h, m, l, d, r = 0, 0, 0, 0, 0
+	j, h, m, l, d, r = 0, 0, 0, 0, 0, 0
+	k = 0
+	if B >= lastelem:
+		k = lastindex
+	else:
+		while primelist[k] < B: # finds index of last prime less than or equal to B
+			k += 1
 	return initialize(N, k, j, B, h, m, l, d, r) # branches to step 1
 
 
@@ -80,7 +86,7 @@ def nextdivisor(N, k, j, B, h, m, l, d, r): # step 5
 	h = (h + 1) % 8
 	d = d + t[h]
 	if d > B:
-		return "remaining divisors are greater than " + B
+		return "remaining divisors are greater than " + str(B)
 	else:
 		return trialdivide(N, k, j, B, h, m, l, d, r) # branches to step 3
 
@@ -89,22 +95,23 @@ def nextdivisor(N, k, j, B, h, m, l, d, r): # step 5
 
 def TrialCompleteFactorization(N, B=lastelem):
 	factorlist = []
-	factor = Trial_Factor(N, B)
+	factor = TrialFactor(N, B)
 	while not isinstance(factor, str):
 		factorlist = factorlist + factor
 		for f in factor:
 			N = int(N / f)
-		factor = Trial_Factor(N, B)
+		factor = TrialFactor(N, B)
 		if N == 1:
 			break
 	if factor == 'prime':
 		factorlist.append(N)
 		return factorlist
 	elif isinstance(factor, str):
-		notprime = factor[len(factor) - 1]
-		return factorlist, notprime
+		return factorlist, 'remaining factors of ' + str(N) + ' are greater than ' + str(B)
 
 
 
-# print(Trial_Complete_Factorization(2065762, 15))
-# print(Trial_Factor(2065762, 15))
+# print(TrialCompleteFactorization(872012557, 100))
+# print(TrialCompleteFactorization(872012557, 1000))
+# print(TrialFactor(2065762, 1500000))
+# print(TrialCompleteFactorization(2065762, 1500000))
