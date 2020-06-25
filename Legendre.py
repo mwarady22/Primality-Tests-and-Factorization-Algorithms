fdcc1dbef9d677d64f8db1adfa20baa8eca761bd
Michaela Warady <mwarady22@berkeley.edu>
2020-06-25 12:05:06 -0700

    comments added to Kronecker in Legendre.py

--- Legendre.py
+++ Legendre.py
@@ -1,7 +1,7 @@
 from sage.all import *
 import sys
 
-def Kronecker(a, b): #
+def Kronecker(a, b): # computes the Kronecker symbol for a, b
 	if b == 0:
 		if (abs(a) == 1):
 			return 1
@@ -26,7 +26,7 @@ def Kronecker(a, b): #
 			k = - k
 	return finishedcheck(a, b, k)
 
-def finishedcheck(a, b, k): # 3
+def finishedcheck(a, b, k):
 	if (a == 0):
 		if b > 1:
 			return 0
@@ -41,7 +41,7 @@ def finishedcheck(a, b, k): # 3
 		k = (- 1)**exp * k
 	return reciprocity(a, b, k)
 
-def reciprocity(a, b, k): # 4
+def reciprocity(a, b, k):
 	exp = (a - 1) * (b - 1) / 4
 	k = (- 1)**exp * k
 	r = abs(a)
