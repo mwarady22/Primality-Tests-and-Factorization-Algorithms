239269050fbf4a7b7e97c6a596cf4799341b82dc
Michaela Warady <mwarady22@berkeley.edu>
2020-06-25 11:57:31 -0700

    progress on Goldwasser_Kilian.py, Kronecker in Legendre.py done

--- Goldwasser_Kilian.py
+++ Goldwasser_Kilian.py
@@ -1,2 +1,68 @@
 from sage.all import *
 import sys
+import random
+from TrialDivisionFactoring import *
+from Rabin_Miller import *
+
+def GKTest(N): #
+	h = 0
+	Ni = N
+	return # 2
+
+def Nismall(): # 2
+	if Ni < 2**30:
+		t = TrialFactor(Ni, 2**15)
+		if not isinstance(t, str):
+			return # 9
+	return # 3
+
+def choosecurve(): # 3
+	a = randint(0, Ni - 1)
+	b = randint(0, Ni - 1)
+	z = 4 * a**3 + 27 * b**2
+	if z % Ni == 0:
+		a, b = choosecurve() #
+	return a, b # 4
+
+
+
+
+def schoof(a, b, Ni): # 4 a, b, Ni
+	ZNiZ = Integers(Ni)
+	E = EllipticCurve(ZNiZ, [a, b])
+	m = E.cardinality()
+	return # 5
+
+
+
+def mcheck(): # 5
+	if (m % 2 == 1):
+		return # 3
+	else:
+		q = m / 2
+
+	rabinmiller = RM(q)
+	if rabinmiller == 'composite':
+		return # 3
+	else:
+		return # 6
+
+
+def findp(): # 6
+	
+
+def checkp(): # 7
+
+def recurse(): # 8
+	h += 1
+	Ni = q
+	return Nismall() #
+
+def backtrack(): # 9
+	if h == 0:
+		return 'composite'
+	else:
+		h -= 1
+		return # 3
+
+# schoof(4, 5, 6)
---
+++ Legendre.py
@@ -0,0 +1,52 @@
+from sage.all import *
+import sys
+
+def Kronecker(a, b): #
+	if b == 0:
+		if (abs(a) == 1):
+			return 1
+		else:
+			return 0
+
+	if (a % 2 == 0) and (b % 2 == 0):
+		return 0
+
+	v = 0
+	while (b % 2 == 0):
+		v += 1
+		b = b / 2
+	if (v % 2 == 0):
+		k = 1
+	else:
+		exp = (a**2 - 1) / 8
+		k = (- 1)**exp
+	if b < 0:
+		b = - b
+		if a < 0:
+			k = - k
+	return finishedcheck(a, b, k)
+
+def finishedcheck(a, b, k): # 3
+	if (a == 0):
+		if b > 1:
+			return 0
+		if b == 1:
+			return int(k)
+	v = 0
+	while (a % 2 == 0):
+		v += 1
+		a = a / 2
+	if (v % 2 == 1):
+		exp = (b**2 - 1) / 8
+		k = (- 1)**exp * k
+	return reciprocity(a, b, k)
+
+def reciprocity(a, b, k): # 4
+	exp = (a - 1) * (b - 1) / 4
+	k = (- 1)**exp * k
+	r = abs(a)
+	a = b % r
+	b = r
+	return finishedcheck(a, b, k)
+
+# print(Kronecker(-1, 5))
