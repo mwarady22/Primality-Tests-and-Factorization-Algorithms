fb0618f346d2d6c7eca3e5473984e23dfb04b69b
Michaela Warady <mwarady22@berkeley.edu>
2020-06-26 10:26:05 -0700

    SqrtModp.py finished, works

--- SqrtModp.py
+++ SqrtModp.py
@@ -12,10 +12,10 @@ def twokcalc(r): # 2kcalc(p) denotes the highest exponent k such that 2**k | t
 	return v
 
 def SqrtModp(a, p): # finds square root mod p for odd prime p
+	a = a % p
 	k = twokcalc(p - 1)
 	q = int((p - 1) / (2**k))
 	while True:
-		print('rand')
 		n = randint(0, p - 1)
 		kr = Kronecker(n, p)
 		if kr == - 1:
@@ -23,9 +23,8 @@ def SqrtModp(a, p): # finds square root mod p for odd prime p
 	z = PowerModm(n, q, p)
 	y = z
 	r = k
-	print('r : ' + str(r))
-	x = PowerModm(a, (q - 1) / 2, p)
-	b = a * PowerModm(x, 2, p)
+	x = PowerModm(a, int((q - 1) / 2), p)
+	b = (a * x**2) % p
 	x = (a * x) % p
 	return findexp(b, p, x, r, a, y) #
 
@@ -34,27 +33,24 @@ def findexp(b, p, x, r, a, y): #
 		return x
 	m = 1
 	twom = 2
-	x = 0
 	while True:
-		print('True : ' + str(x))
 		b2m = PowerModm(b, twom, p)
 		if b2m == 1:
 			break
 		m += 1
 		twom *= 2
-		x += 1
 	if m == r:
 		return str(a) + ' is not a quadratic residue mod ' + str(p)
 	return redexp(m, y, b, p, x, r, a) #
 
 def redexp(m, y, b, p, x, r, a): #
-	exp = r - m - 1
-	print('exp : ' + str(exp))
-	t = LRbin(y, exp, calculate_e(exp))
-	y = t**2
-	r = m
-	x = x * t
-	b = b * y
+	expexp = r - m - 1
+	exp = LRbin(2, expexp, calculate_e(expexp))
+	t = PowerModm(y, exp, p)
+	y = (t**2) % p
+	r = m % p
+	x = (x * t) % p
+	b = (b * y) % p
 	return findexp(b, p, x, r, a, y) 
 
-print(SqrtModp(8, 17))
+# print(SqrtModp(109, 71))
