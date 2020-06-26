7f52bbad55d6b07d0075beacb5af7a88d6bb7005
Michaela Warady <mwarady22@berkeley.edu>
2020-06-26 10:33:59 -0700

    commented SqrtModp.py

--- SqrtModp.py
+++ SqrtModp.py
@@ -12,10 +12,10 @@ def twokcalc(r): # 2kcalc(p) denotes the highest exponent k such that 2**k | t
 	return v
 
 def SqrtModp(a, p): # finds square root mod p for odd prime p
-	a = a % p
+	a = a % p # reduce a so as to make calculations faster
 	k = twokcalc(p - 1)
-	q = int((p - 1) / (2**k))
-	while True:
+	q = int((p - 1) / (2**k)) # p - 1 = 2**k * q
+	while True: # find n such that (n / p) = - 1
 		n = randint(0, p - 1)
 		kr = Kronecker(n, p)
 		if kr == - 1:
@@ -26,14 +26,14 @@ def SqrtModp(a, p): # finds square root mod p for odd prime p
 	x = PowerModm(a, int((q - 1) / 2), p)
 	b = (a * x**2) % p
 	x = (a * x) % p
-	return findexp(b, p, x, r, a, y) #
+	return findexp(b, p, x, r, a, y)
 
-def findexp(b, p, x, r, a, y): #
+def findexp(b, p, x, r, a, y):
 	if (b % p) == 1:
-		return x
+		return x # found square root
 	m = 1
 	twom = 2
-	while True:
+	while True: # find smallest b**(2**m) % p = 1
 		b2m = PowerModm(b, twom, p)
 		if b2m == 1:
 			break
@@ -41,12 +41,12 @@ def findexp(b, p, x, r, a, y): #
 		twom *= 2
 	if m == r:
 		return str(a) + ' is not a quadratic residue mod ' + str(p)
-	return redexp(m, y, b, p, x, r, a) #
+	return redexp(m, y, b, p, x, r, a)
 
-def redexp(m, y, b, p, x, r, a): #
+def redexp(m, y, b, p, x, r, a):
 	expexp = r - m - 1
 	exp = LRbin(2, expexp, calculate_e(expexp))
-	t = PowerModm(y, exp, p)
+	t = PowerModm(y, exp, p) # t = y**(2**(r - m - 1))
 	y = (t**2) % p
 	r = m % p
 	x = (x * t) % p
