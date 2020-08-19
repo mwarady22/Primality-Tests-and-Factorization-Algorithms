c85cbcc8f1ccfa8d779f4a45e1c564d35059d340
Michaela Warady <mwarady22@berkeley.edu>
2020-08-19 16:19:48 -0700

    MPQS.py .py commented

--- MPQS.py
+++ MPQS.py
@@ -8,22 +8,21 @@ def MPQS(N, f, M):
 	baselist, baseproduct, largestbaseindex = collectbase(N, f) # list of base elements, product of all base elements, index in primelist of largest element
 	sqrts = [] # will hold the modular square roots of each odd prime in the baselist, with None at index 0
 	logs = []# will hold the base 2 log of each prime in the baselist
-	mid = sqrt((sqrt(2 * N) / M))
-	for p in baselist:
+	for p in baselist: # fill sqrts and logs
 		if p == 2:
 			sqrts.append(1)
 			logs.append(1)
 		else:
 			sqrts.append(SqrtModp(N, p))
 			logs.append(log(p, 2).n())
-	qindex, q = chooseq(N, M, largestbaseindex)
+	qindex, q = chooseq(N, M, largestbaseindex) # find an optimal q and its index in primelist
 	if q == None:
 		return 'MPQS will not work on ' + str(N)
-	plusindex = qindex
-	minusindex = qindex - 1
-	last = 'minus'
+	plusindex = qindex # index radiating out from q in the positive direction
+	minusindex = qindex - 1 # index radiating out from q in the negative direction
+	last = 'minus' # indicates which direction the last index was taken from
 	while True:
-		if minusindex > largestbaseindex:
+		if minusindex > largestbaseindex: # make sure Kronecker(N, q) == 1
 			if Kronecker(N, primelist[minusindex]) == 1:
 				break
 			else:
@@ -34,7 +33,7 @@ def MPQS(N, f, M):
 	return newpoly(N, M, baselist, baseproduct, largestbaseindex, sqrts, logs, plusindex, minusindex, last)
 
 def newpoly(N, M, baselist,baseproduct, largestbaseindex, sqrts, logs, plusindex, minusindex, last):
-	if last == 'minus':
+	if last == 'minus': # last q was taken in the negative direction, now use positive directions
 		qindex = plusindex
 		last = 'plus'
 		plusindex += 1
@@ -42,7 +41,7 @@ def newpoly(N, M, baselist,baseproduct, largestbaseindex, sqrts, logs, plusindex
 			plusindex += 1
 		if plusindex >= len(primelist):
 			last = 'stayminus'
-	elif last == 'plus':
+	elif last == 'plus': # last q was taken in the positive direction, now use negative directions
 		qindex = minusindex
 		last = 'minus'
 		minusindex -= 1
@@ -50,25 +49,25 @@ def newpoly(N, M, baselist,baseproduct, largestbaseindex, sqrts, logs, plusindex
 			minusindex -= 1
 		if minusindex < 0:
 			last = 'stayplus'
-	elif last == 'stayplus':
+	elif last == 'stayplus': # all q from now on taken in the positive direction
 		qindex = plusindex
 		plusindex += 1
 		while plusindex < len(primelist) and Kronecker(N, primelist[plusindex]) != 1:
 			plusindex += 1
-	elif last == 'stayminus':
+	elif last == 'stayminus': # all q from now on taken in the negative direction
 		qindex = minusindex
 		minusindex -= 1
 		while minusindex > largestbaseindex and Kronecker(N, primelist[minusindex]) != 1:
 			minusindex -= 1
-	if (last == 'stayplus') and (qindex >= len(primelist)):
+	if (last == 'stayplus') and (qindex >= len(primelist)): # we have used all available qs
 		return 'cannot be solved with given bounds'
-	if (last == 'stayminus') and (qindex < 0):
+	if (last == 'stayminus') and (qindex < 0): # we have used all available qs
 		return 'cannot be solved with given bounds'
 	q = primelist[qindex]
-	a = q**2
-	b = int(mod(N, a).sqrt())
-	c = int((b**2 - N) / a)
-	sollist = []
+	a = q**2 # a in a * x**2 + b * x + c
+	b = int(mod(N, a).sqrt()) # b in a * x**2 + b * x + c
+	c = int((b**2 - N) / a) # c in a * x**2 + b * x + c
+	sollist = [] # will hold solutions to a * x**2 + b * x + c mod p for each p in baselist
 	for pindex in range(0, len(baselist)):
 		p = baselist[pindex]
 		sqrtp = sqrts[pindex]
@@ -77,7 +76,7 @@ def newpoly(N, M, baselist,baseproduct, largestbaseindex, sqrts, logs, plusindex
 		if p != 2:
 			soln1 = (ainv * (- sqrtp - b)) % p
 			sollist.append([soln0, soln1])
-		else:
+		else: # p == 2 has only one solution
 			sollist.append([soln0])
 	return sieve(N, M, baselist, baseproduct, largestbaseindex, sqrts, logs, plusindex, minusindex, last, a, b, c, sollist)
 
@@ -86,57 +85,56 @@ def sieve(N, M, baselist, baseproduct, largestbaseindex, sqrts, logs, plusindex,
 	sneg = [0] * (M + 1) # holds 0 and negative indices for sieving
 	for pindex in range(0, len(baselist)):
 		p = baselist[pindex]
-		sol0 = sollist[pindex][0]
+		sol0 = sollist[pindex][0] # first solution mod p
 		indpos = sol0
 		if sol0 == 0:
 			indneg = 0
 		else:
 			indneg = - (sol0 - p)
-		while indpos < M + 1:
+		while indpos < M + 1: # while in range, add logs[pindex]
 			spos[indpos] = spos[indpos] + logs[pindex]
-			indpos += p
-		while indneg < M + 1:
+			indpos += p # do this every p indices
+		while indneg < M + 1: # while in range, add logs[pindex]
 			sneg[indneg] = sneg[indneg] + logs[pindex]
-			indneg += p
-		if p != 2:
+			indneg += p # do this every p indices
+		if p != 2: # repeat with other solution if p != 2
 			sol1 = sollist[pindex][1]
 			indpos = sol1
 			indneg = - (sol1 - p)
-			while indpos < M + 1:
+			while indpos < M + 1: # while in range, add logs[pindex]
 				if indpos >= 0:
 					spos[indpos] = spos[indpos] + logs[pindex]
-					indpos += p
-			while indneg < M + 1:
+					indpos += p # do this every p indices
+			while indneg < M + 1: # while in range, add logs[pindex]
 				if indneg >= 0:
 					sneg[indneg] = sneg[indneg] + logs[pindex]
-					indneg += p
+					indneg += p # do this every p indices
 	return checksieve(N, M, baselist, baseproduct, largestbaseindex, sqrts, logs, plusindex, minusindex, last, a, b, c, sollist, spos, sneg)
 
 def checksieve(N, M, baselist, baseproduct, largestbaseindex, sqrts, logs, plusindex, minusindex, last, a, b, c, sollist, spos, sneg):
-	check = log((M * sqrt(N)), 2).n() - 10
-	Q = []
-	R = []
+	check = log((M * sqrt(N)), 2).n() - 10 # number over which we will perform bsmoothchecks
+	Q = [] # will hold index, evaluated polynomial pairs
+	R = [] # will hold the base factorization along with its index in R
 	x = ZZ['x'].gen()
 	g = a * x**2 + 2 * b * x + c
 	for indpos in range(0, len(spos)):
-		if spos[indpos] >= check:
+		if spos[indpos] >= check: # if it is likely that gx is bsmooth check if it is
 			gx = g(indpos)
 			smooth = bsmoothcheck(gx, baseproduct)
-			if smooth == 1:
+			if smooth == 1: # if it is bsmooth, put it in R, Q
 				R.append([[len(R)], basefactorize(gx, baselist)])
 				Q.append([indpos, gx])
 	for indneg in range(1, len(sneg)):
-		if sneg[indneg] >= check:
+		if sneg[indneg] >= check: # if it is likely that gx is bsmooth check if it is
 			gx = g(indneg)
 			smooth = bsmoothcheck(gx, baseproduct)
-			if smooth == 1:
+			if smooth == 1: # if it is bsmooth, put it in R, Q
 				R.append([[len(R)], basefactorize(gx, baselist)])
 				Q.append([- indneg, gx])
 	dep = lindep(N, Q, R) # factorization or None if we cannot yet find a factorization
 	if dep != None:
 		return dep # the factorization
 	else:
-		# return
 		return newpoly(N, M, baselist, baseproduct, largestbaseindex, sqrts, logs, plusindex, minusindex, last) # didn't work, try new polynomial
 
 def bsmoothcheck(t, q): # returns 1 if t is bsmooth over base and 0 otherwise
@@ -227,10 +225,10 @@ def factorize(N, x2, y): # find factorization of N
 
 def chooseq(N, M, largestbaseindex):
 	close = int(sqrt((sqrt(2 * N)) / M)) # find prime q close to this number which is a quadratic residue mod N
-	if close <= primelist[largestbaseindex]:
+	if close <= primelist[largestbaseindex]: # find q above highest p in baselist
 		pindex = largestbaseindex + 1
 		kronindex = None
-		while True:
+		while True: # find first prime larger than close for which Kronecker gives 1
 			if Kronecker(N, primelist[pindex]) == 1:
 				kronindex = pindex
 				break
@@ -238,14 +236,14 @@ def chooseq(N, M, largestbaseindex):
 		return pindex, primelist[pindex]
 	else:
 		pindex = 0
-		while primelist[pindex] < close:
+		while primelist[pindex] < close: # find q larger than close
 			pindex += 1
-		if Kronecker(N, primelist[pindex]):
+		if Kronecker(N, primelist[pindex]): # if this q has a Kronecker value of 1, return and use it
 			return pindex, primelist[pindex]
 		else:
 			pindexminus = pindex - 1
 			pindexplus = pindex + 1
-			while True:
+			while True: # find closest q to close that has a Kronecker value of 1
 				if (pindexminus < largestbaseindex) and (pindexplus >= len(primelist)):
 					return 'cannot find usable q', None
 				if pindexminus > largestbaseindex:
