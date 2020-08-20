c2d09ed2b06040a8c07d274a9e064e8c98a087d7
Michaela Warady <mwarady22@berkeley.edu>
2020-08-19 17:57:08 -0700

    WilsonsTheorem.py finished

--- WilsonsTheorem.py
+++ WilsonsTheorem.py
@@ -3,12 +3,10 @@ import sys
 
 def Wilson(p):
 	ans = 1
-	for j in range(1, p): # compute (p - 1)! % p
+	for j in range(1, p): # compute (p - 1)! mod p
 		ans *= j
 		ans = ans % p
-	if ans == (p - 1):
+	if ans == (p - 1): # if (p - 1)! == - 1 mod p, p is prime
 		return 'prime'
-	else:
+	else: # otherwise p is composite
 		return 'composite'
-
-# print(Wilson(101))
